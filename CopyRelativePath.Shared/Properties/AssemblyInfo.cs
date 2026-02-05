237865e37f9af6a60da50fdbb4e0bd71030a1552
abevol <85929368+abevol@users.noreply.github.com>
2026-02-05 18:42:58 +0800

    chore: bump version to 1.4.0
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- CopyRelativePath.Shared/Properties/AssemblyInfo.cs
+++ CopyRelativePath.Shared/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) mere-human. All rights reserved.
+// Copyright (c) mere-human. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Reflection;
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.4.*")]
