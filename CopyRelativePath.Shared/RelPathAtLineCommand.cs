f3e6bb7a6d7dba2d74dc0e5aa00b975b1d93f185
abevol <85929368+abevol@users.noreply.github.com>
2026-02-05 18:43:20 +0800

    feat: add copy relative path with line number commands
    - Copy Current Line &Relative Path
    
    - Copy Selected Lines &Relative Path
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ CopyRelativePath.Shared/RelPathAtLineCommand.cs
@@ -0,0 +1,86 @@
+// Copyright (c) mere-human. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using System.Windows.Forms;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyRelativePath
+{
+    /// <summary>
+    /// Command handler for Copy Current Line &Relative Path
+    /// </summary>
+    internal sealed class RelPathAtLineCommand : BaseCopyCommand
+    {
+        public const int CommandId = 0x0500;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("31ffadf9-d4ce-44e3-8931-03823256b328");
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelPathAtLineCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private RelPathAtLineCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            base.package = package as ExtensionPackage ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static RelPathAtLineCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in RelPathAtLineCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new RelPathAtLineCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string filePath = GetRelPath();
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                if (package.DTE.ActiveDocument != null)
+                {
+                    var textSel = package.DTE.ActiveDocument.Object("TextSelection") as TextSelection;
+                    if (textSel != null)
+                    {
+                        filePath += FormatLineNumber(textSel.CurrentLine, package.OptionLinePathSuffix);
+                        Clipboard.SetText(filePath);
+                    }
+                }
+            }
+        }
+    }
+}
